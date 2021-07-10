659f4783faf1b246d2f4317fbf469d2eb17062b5
moonative <minssu913@naver.com>
2021-07-10 22:09:45 +0900

    add delete-button of toDoList

--- js/todo.js
+++ js/todo.js
@@ -3,15 +3,28 @@ const toDoInput=toDoForm.querySelector("input")
 //const toDoInput=document.querySelector("#todo-form input"); 위와 동일.
 const toDoList=document.getElementById("todo-list");
 
+const a=localStorage.getItem("todo");
 
 function paintToDo(newTodo){
+    /********list*********/
     const li=document.createElement("li");
     const span=document.createElement("span");
     li.appendChild(span);
     span.innerText=newTodo;
+    /********button*********/
+    const button=document.createElement("button");
+    li.appendChild(button);
+    button.innerText="❌";
+    /********list&button 합침*********/
     toDoList.appendChild(li);
 
-  
+    /********delete button*********/
+    button.addEventListener("click",(e)=>{
+        li_form=e.target.parentNode;//console.dir(e.target)으로 parent를 찾음.
+        li_form.remove();
+    })
+    localStorage.setItem("todo",li);
+
 }
 
 function handleToDoSubmit(e){
