91fa4a08e57d01b251cb0f5aa1160ecb0784caa4
moonative <minssu913@naver.com>
2021-07-10 21:54:40 +0900

    add li of toDoList

---
+++ js/todo.js
@@ -0,0 +1,23 @@
+const toDoForm=document.getElementById("todo-form");
+const toDoInput=toDoForm.querySelector("input")
+//const toDoInput=document.querySelector("#todo-form input"); 위와 동일.
+const toDoList=document.getElementById("todo-list");
+
+
+function paintToDo(newTodo){
+    const li=document.createElement("li");
+    const span=document.createElement("span");
+    li.appendChild(span);
+    span.innerText=newTodo;
+    toDoList.appendChild(li);
+
+  
+}
+
+function handleToDoSubmit(e){
+    e.preventDefault();
+    const newTodo = toDoInput.value;
+    toDoInput.value="";
+    paintToDo(newTodo);
+}
+toDoForm.addEventListener("submit",handleToDoSubmit);
