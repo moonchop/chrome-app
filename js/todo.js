254005d0143f45afea7bd32ac46861d04846e954
moonative <minssu913@naver.com>
2021-07-10 23:19:45 +0900

    add loding-list of toDoList

--- js/todo.js
+++ js/todo.js
@@ -3,9 +3,17 @@ const toDoInput=toDoForm.querySelector("input")
 //const toDoInput=document.querySelector("#todo-form input"); 위와 동일.
 const toDoList=document.getElementById("todo-list");
 
-const a=localStorage.getItem("todo");
+let toDos=[];
 
-function paintToDo(newTodo){
+
+function saveToDos(){
+    //localStorage.setItem("todos",toDos);
+    //처음엔 list 각각을 setItem 해주어서 따로 관리하려고 했음. But key 하나에 value를 배열로 관리하면됨.  
+    localStorage.setItem("todos",JSON.stringify(toDos)); //list를 string으로 바꿈.
+   
+}
+
+function paintToDo(newTodo){ //list를 만들어주는 function
     /********list*********/
     const li=document.createElement("li");
     const span=document.createElement("span");
@@ -20,10 +28,11 @@ function paintToDo(newTodo){
 
     /********delete button*********/
     button.addEventListener("click",(e)=>{
-        li_form=e.target.parentNode;//console.dir(e.target)으로 parent를 찾음.
+        li_form=e.target.parentNode;//console.dir(e.target)으로 parent를 찾음(li의 부모에게 접근해서 list와 버튼을 삭제)
         li_form.remove();
-    })
-    localStorage.setItem("todo",li);
+    });
+
+    
 
 }
 
@@ -31,6 +40,20 @@ function handleToDoSubmit(e){
     e.preventDefault();
     const newTodo = toDoInput.value;
     toDoInput.value="";
+    toDos.push(newTodo);
     paintToDo(newTodo);
+    saveToDos();
 }
 toDoForm.addEventListener("submit",handleToDoSubmit);
+
+const savedToDos=localStorage.getItem("todos");
+//savedToDos는 단지 string.
+if(savedToDos){
+    const parsedToDos=JSON.parse(savedToDos);
+     //parsedToDos는 단순한 strign을 parse를 통해 실제로 무언가를 할 수 있는 배열을 얻는다.
+    toDos=parsedToDos; //list를 추가해서 새로고침하면, 전에 list들을 기억하기 위함.
+    //setItem해주기 전에 toDos의 빈배열에 
+    console.log(toDos);
+
+     parsedToDos.forEach(paintToDo); //parsedToDos에 있는 인자들을 paintToDo에 넣어주기 때문에 paintToDo의 인자를 따로 넣어주지 않는다.
+}
